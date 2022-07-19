a44db447452d4e37386c519a412f5d248b76fae7
Kavinprasath Mathaiyan <kavinprasath.mathaiyan@siriuscomservices.com>
2022-07-19 12:15:45 +0530

    login page function update

--- foodie-react/src/App.js
+++ foodie-react/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import {  BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RestaurantListPage from "./pages/RestaurantListPage";
 import OrdersPage from "./pages/OrdersPage";
@@ -8,20 +8,10 @@ import OrdersPage from "./pages/OrdersPage";
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route exact path={"/"} element={<LoginPage />}>
-      </Route>
-      <Route
-        exact
-        path={"/restaurantList"}
-        element={<RestaurantListPage />}
-      ></Route>
-      <Route
-        exact
-        path={"/orders/:id"}
-        element={<OrdersPage />}
-      >
-      </Route>
+      <Routes>
+        <Route exact path={"/restaurantList"} element={<RestaurantListPage />}></Route>
+        <Route exact path={"/orders/:id"} element={<OrdersPage />}></Route>
+        <Route path={"*"} element={<LoginPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- foodie-react/src/pages/LoginPage.js
+++ foodie-react/src/pages/LoginPage.js
@@ -1,60 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/loginPageStyles.css";
 import TextInput from "../components/TextInput";
 import CommonButton from "../components/CommonButton";
 import logo from "../resources/images/logo.png";
 
-export default class LoginPage extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      userName: "",
-      password: ""
-    };
-  }
-
-  render() {
-    return (
-      <div className="main-container login-page">
-        <div className="login-background"></div>
-        <div className="login-form">
-          <div className="app-logo">
-            <img src={logo} alt="chicken tikka"/>
-          </div>
-          <form className="login-form" action="">
-            <h1>Sign In</h1>
-            <div className="form-field">
-              <TextInput
-                type="text"
-                className="username"
-                placeholder="Username"
-                value={this.state.userName}
-                onChange={event =>
-                  this.setState({ userName: event.target.value })
-                }
-              />
-            </div>
-            <div className="form-field">
-              <TextInput
-                type="password"
-                className="password"
-                placeholder="Password"
-                value={this.state.password}
-                onChange={event =>
-                  this.setState({ password: event.target.value })
-                }
-              />
-            </div>
-            <div className="form-field">
-              <CommonButton className="primary" type="submit" value="LOGIN" />
-            </div>
-            <span className="forgot-password">Forgot Password?</span>
-            <a className="link" href="*">
-              Click Here
-            </a>
-          </form>
+const LoginPage = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="main-container login-page">
+      <div className="login-background"></div>
+      <div className="login-form">
+        <div className="app-logo">
+          <img src={logo} alt="chicken tikka" />
         </div>
+        <form className="login-form" action="">
+          <h1>Sign In</h1>
+          <div className="form-field">
+            <TextInput
+              type="text"
+              className="username"
+              placeholder="Username"
+              value={userName}
+              onChange={(event) =>  setUserName(event.target.value)}
+            />
+          </div>
+          <div className="form-field">
+            <TextInput
+              type="password"
+              className="password"
+              placeholder="Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <div className="form-field">
+            <CommonButton className="primary" type="submit" value="LOGIN" />
+          </div>
+          <span className="forgot-password">Forgot Password?</span>
+          <a className="link" href="*">
+            Click Here
+          </a>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default LoginPage;
