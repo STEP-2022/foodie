0abacda0be457bfcd19260db21d77ccedd0b1003
Kavinprasath Mathaiyan <kavinprasath.mathaiyan@siriuscomservices.com>
2022-07-19 15:49:38 +0530

    null check added

--- foodie-react/src/components/OrdersView.js
+++ foodie-react/src/components/OrdersView.js
@@ -8,15 +8,15 @@ const OrdersView = (props) => {
     <div className="items">
       <div className="restaurant">
         <div className="child">
-          <h1 className="title">{restaurant.name}</h1>
-          <div className="address">{restaurant.address}</div>
-          <div className="address">{restaurant.cuisines}</div>
+          <h1 className="title">{restaurant?.name}</h1>
+          <div className="address">{restaurant?.address}</div>
+          <div className="address">{restaurant?.cuisines}</div>
         </div>
       </div>
       <div className="order-items">
         <div className="child">
           <h1 className="title">Order Now</h1>
-          {restaurant.menu
+          {restaurant?.menu
             ? restaurant.menu.map((item, index) => (
                 <OrderItem key={index} item={item} onAddToCart={onAddToCart} />
               ))
