785f259995cfb3456bccf04dad6342ca4f505844
Kavinprasath Mathaiyan <kavinprasath.mathaiyan@siriuscomservices.com>
2022-07-19 15:12:22 +0530

    functional approach update

--- foodie-react/src/pages/RestaurantListPage.js
+++ foodie-react/src/pages/RestaurantListPage.js
@@ -1,63 +1,49 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/restaurantListPageStyles.css";
 import HeaderBar from "../components/HeaderBar";
 import { RestaurantService } from "../services/RestaurantService";
 import RestaurantCard from "../components/RestaurantCard";
 
-export class RestaurantListPage extends Component {
-  constructor(props) {
-    super(props);
+const RestaurantListPage = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
 
-    this.state = {
-      isLoading: false,
-      error: null,
-      data: []
+  useEffect(() => {
+    /**
+     * Fetch the list of restaurants from RestaurantService and updates the component state.
+     * Sets the state to display loading indicator during data fetch from service.
+     */
+    const fetchRestaurantList = async () => {
+      setLoading(true);
+      let restaurantsList = await RestaurantService.getRestaurantsList();
+      if (restaurantsList.length > 0) {
+        setData(restaurantsList);
+        setLoading(false);
+        setError(null);
+      } else {
+        setData(restaurantsList);
+        setLoading(false);
+        setError("No Data Present!");
+      }
     };
-  }
+    fetchRestaurantList();
+  }, []);
 
-  componentDidMount() {
-    this.fetchRestaurantList();
-  }
-
-  /**
-   * Fetch the list of restaurants from RestaurantService and updates the component state.
-   * Sets the state to display loading indicator during data fetch from service.
-   */
-  async fetchRestaurantList() {
-    this.setState({ isLoading: true });
-    let restaurantsList = await RestaurantService.getRestaurantsList();
-    if (restaurantsList.length > 0) {
-      this.setState({
-        isLoading: false,
-        data: restaurantsList,
-        error: null
-      });
-      console.log(this.state);
-    } else {
-      this.setState({
-        isLoading: false,
-        data: restaurantsList,
-        error: "No Data Present!"
-      });
-    }
-  }
-
-  render() {
-    return (
-      <div className="restaurant-list-container">
-        <HeaderBar />
-        <div className="restaurant-list-wrapper child">
-          {this.state.data.map(res => {
-            return (
-              <div key={res.id + res.name}>
-                <RestaurantCard key={res.id} restaurant={res} />
-              </div>
-            );
-          })}
-        </div>
+  return (
+    <div className="restaurant-list-container">
+      <HeaderBar />
+      <div className="restaurant-list-wrapper child">
+        {data.map((res) => {
+          return (
+            <div key={res.id + res.name}>
+              <RestaurantCard key={res.id} restaurant={res} />
+            </div>
+          );
+        })}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default RestaurantListPage;
